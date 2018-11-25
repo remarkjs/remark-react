bb8f54128cec02cbc1bbb1cec636c283397af6a0
Titus Wormer <tituswormer@gmail.com>
2018-11-25 18:15:23 +0100

    Refactor to use tape

--- test/index.js
+++ test/index.js
@@ -1,10 +1,8 @@
 'use strict'
 
-/* eslint-env mocha */
-
 var path = require('path')
 var fs = require('fs')
-var assert = require('assert')
+var test = require('tape')
 var remark = require('remark')
 var frontmatter = require('remark-frontmatter')
 var vfile = require('vfile')
@@ -12,74 +10,62 @@ var hidden = require('is-hidden')
 var not = require('not')
 var reactRenderer = require('..')
 
-var read = fs.readFileSync
-var write = fs.writeFileSync
-var exists = fs.existsSync
-var join = path.join
+var versions = ['v16']
 
-;['v16'].forEach(function(reactVersion) {
+versions.forEach(function(reactVersion) {
   var React = require(path.join(__dirname, 'react', reactVersion))
 
-  var root = join(__dirname, 'react', reactVersion, 'fixtures')
+  var root = path.join(__dirname, 'react', reactVersion, 'fixtures')
   var fixtures = fs.readdirSync(root)
 
   fixtures = fixtures.filter(not(hidden))
 
-  describe('on React ' + reactVersion, function() {
-    describe('remark-react', function() {
-      it('should be a function', function() {
-        assert(typeof reactRenderer === 'function')
-      })
+  test('React ' + reactVersion, function(t) {
+    t.doesNotThrow(function() {
+      remark()
+        .use(reactRenderer)
+        .freeze()
+    }, 'should not throw if not passed options')
 
-      it('should not throw if not passed options', function() {
-        assert.doesNotThrow(function() {
+    t.test('should use consistent keys on multiple renders', function(st) {
+      var markdown = '# A **bold** heading'
+
+      st.deepEqual(reactKeys(markdown), reactKeys(markdown))
+
+      st.end()
+
+      function reactKeys(text) {
+        return extractKeys(
           remark()
-            .use(reactRenderer)
-            .freeze()
-        })
-      })
-
-      it('should use consistent React keys on multiple renders', function() {
-        function extractKeys(reactElement) {
-          var keys = []
-
-          if (reactElement.key != null) {
-            keys = keys.concat(reactElement.key)
-          }
-
-          if (reactElement.props != null) {
-            var childKeys = []
-
-            React.Children.forEach(reactElement.props.children, function(
-              child
-            ) {
-              childKeys = childKeys.concat(extractKeys(child))
-            })
-
-            keys = keys.concat(childKeys)
-          }
-
-          return keys
-        }
-
-        function reactKeys(text) {
-          var vdom = remark()
             .use(reactRenderer, {createElement: React.createElement})
             .processSync(text).contents
-          return extractKeys(vdom)
+        )
+      }
+
+      function extractKeys(reactElement) {
+        var keys = []
+
+        if (reactElement.key != null) {
+          keys = keys.concat(reactElement.key)
         }
 
-        var markdown = '# A **bold** heading'
-        var keys1 = reactKeys(markdown)
-        var keys2 = reactKeys(markdown)
+        if (reactElement.props != null) {
+          var childKeys = []
 
-        assert.deepStrictEqual(keys1, keys2)
-      })
+          React.Children.forEach(reactElement.props.children, function(child) {
+            childKeys = childKeys.concat(extractKeys(child))
+          })
 
-      it('should use custom components', function() {
-        var markdown = '# Foo'
+          keys = keys.concat(childKeys)
+        }
 
-        var vdom = remark()
+        return keys
+      }
+    })
+
+    t.equal(
+      React.renderToStaticMarkup(
+        remark()
           .use(reactRenderer, {
             createElement: React.createElement,
             remarkReactComponents: {
@@ -88,95 +74,69 @@ var join = path.join
               }
             }
           })
-          .processSync(markdown).contents
+          .processSync('# Foo').contents
+      ),
+      '<div><h2>Foo</h2></div>',
+      'should use custom components'
+    )
 
-        assert.strictEqual(
-          React.renderToStaticMarkup(vdom),
-          '<div><h2>Foo</h2></div>'
-        )
-      })
-
-      it('does not sanitize input when `sanitize` option is set to false', function() {
-        var markdown = '```empty\n```'
-        var vdom = remark()
+    // If sanitation were done, 'class' property should be removed.
+    t.equal(
+      React.renderToStaticMarkup(
+        remark()
           .use(reactRenderer, {
             createElement: React.createElement,
             sanitize: false
           })
-          .processSync(markdown).contents
+          .processSync('```empty\n```').contents
+      ),
+      '<div><pre><code class="language-empty"></code></pre></div>',
+      'does not sanitize input when `sanitize` option is set to false'
+    )
 
-        // If sanitation were done, 'class' property should be removed.
-        assert.strictEqual(
-          React.renderToStaticMarkup(vdom),
-          '<div><pre><code class="language-empty"></code></pre></div>'
-        )
-      })
-
-      it('passes toHast options to inner toHAST() function', function() {
-        var markdown = '# Foo'
-
-        var vdom = remark()
+    t.equal(
+      React.renderToStaticMarkup(
+        remark()
           .use(reactRenderer, {
             createElement: React.createElement,
             toHast: {allowDangerousHTML: true}
           })
-          .processSync(markdown).contents
+          .processSync('# Foo').contents
+      ),
+      '<div><h1>Foo</h1></div>',
+      'passes toHast options to inner toHAST() function'
+    )
 
-        assert.strictEqual(
-          React.renderToStaticMarkup(vdom),
-          '<div><h1>Foo</h1></div>'
-        )
-      })
-    })
+    fixtures.forEach(function(name) {
+      var base = path.join(root, name)
+      var input = fs.readFileSync(path.join(base, 'input.md'))
+      var expected = fs.readFileSync(path.join(base, 'output.html'), 'utf8')
+      var config
+      var actual
 
-    function describeFixture(fixture) {
-      it('should work on `' + fixture + '`', function() {
-        var filepath = join(root, fixture)
-        var output = read(join(filepath, 'output.html'), 'utf-8')
-        var input = read(join(filepath, 'input.md'), 'utf-8')
-        var config = join(filepath, 'config.json')
-        var file = vfile({path: fixture + '.md', contents: input})
-        var result
-
-        config = exists(config) ? JSON.parse(read(config, 'utf-8')) : {}
-        config.createElement = React.createElement
-        result = processSync(file, config)
-
-        if (global.process.env.UPDATE) {
-          write(join(filepath, 'output.html'), result)
-        }
-
-        assertion(result, output)
-      })
-    }
-
-    describe('Fixtures', function() {
-      fixtures.forEach(describeFixture)
-    })
-  })
-
-  function assertion(actual, expected, silent) {
-    try {
-      assert(actual === expected)
-    } catch (error) {
-      error.expected = expected
-      error.actual = actual
-
-      if (silent) {
-        return error
+      try {
+        config = JSON.parse(fs.readFileSync(path.join(base, 'config.json')))
+      } catch (error) {
+        config = {}
       }
 
-      throw error
-    }
-  }
+      config.createElement = React.createElement
 
-  // Shortcut to process.
-  function processSync(file, config) {
-    var vdom = remark()
-      .data('settings', config)
-      .use(frontmatter)
-      .use(reactRenderer, config)
-      .processSync(file).contents
-    return React.renderToStaticMarkup(vdom)
-  }
+      actual = React.renderToStaticMarkup(
+        remark()
+          .data('settings', config)
+          .use(frontmatter)
+          .use(reactRenderer, config)
+          .processSync(vfile({path: name + '.md', contents: input})).contents
+      )
+
+      if (global.process.env.UPDATE) {
+        fs.writeFileSync(path.join(root, name, 'output.html'), actual)
+      }
+
+      t.equal(actual, expected, name)
+    })
+
+    t.end()
+  })
 })
